1bed936b04c4d43d66ba84622e4149bcbe647f9b
Leal.li <leal.li@aishu.cn>
2021-12-27 16:44:09 +0800

    黎彬彬: 【feature-arp-137895】清理无用的测试代码

--- src/test/java/com/eisoo/telemetry/SamplerLoggerTest.java
+++ src/test/java/com/eisoo/telemetry/SamplerLoggerTest.java
@@ -15,8 +15,6 @@ import io.opentelemetry.semconv.resource.attributes.ResourceAttributes;
 import org.junit.Assert;
 import org.junit.Test;
 
-import java.util.regex.Pattern;
-
 
 public class SamplerLoggerTest {
 
