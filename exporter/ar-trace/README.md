d6d3df16a6e3084786f9f85403ba32744c7cad90
Leal.li <Leal.li@aishu.cn>
2023-03-09 17:13:04 +0800

    [feature-arp-331612]Readme添加跨函数调用的例子

--- exporter/ar-trace/README.md
+++ exporter/ar-trace/README.md
@@ -1,6 +1,8 @@
 # 开发指南
 
-本项目生成包含所有依赖包的命令： mvn clean install assembly:assembly -DskipTests
+本项目生成包含所有依赖包的命令： 
+由于本项目依赖：opentelemetry-exporter-common，需要到common项目里先安装好：mvn clean install
+再回到本项目生成两个jar包：mvn clean install assembly:assembly -DskipTests
 
 
 ## 导入TelemetrySDK-Trace(Java)
@@ -84,5 +86,60 @@ public class TraceExporterTest {
         span.end();
     }
 }
+
+
+//跨函数调用：
+ public  void testChain () {
+        final Resource serviceNameResource =
+                Resource.create(io.opentelemetry.api.common.Attributes.of(ResourceAttributes.SERVICE_NAME, "otel-jaeger-example2"));
+
+        // Set to process the spans by the Jaeger Exporter
+        final SdkTracerProvider tracerProvider =
+                SdkTracerProvider.builder()
+                        .addSpanProcessor(SimpleSpanProcessor.create(new ArExporter()))
+
+                        //发送到anyRobot：
+//                    .addSpanProcessor(SimpleSpanProcessor.create(ArExporter.create(
+//                                HttpSender.create("http://10.4.15.62/api/feed_ingester/v1/jobs/job-0988e01371fd21c9/events",
+//                                        Retry.create(true,5,15,30),
+//                                        true, 4096))))
+
+                        .setResource(Resource.getDefault().merge(serviceNameResource))
+                        .build();
+        final OpenTelemetrySdk openTelemetry =
+                OpenTelemetrySdk.builder().setTracerProvider(tracerProvider).build();
+
+        // it's always a good idea to shut down the SDK cleanly at JVM exit.
+//    Runtime.getRuntime().addShutdownHook(new Thread(tracerProvider::close));
+        Tracer tracer = openTelemetry.getTracer("io.opentelemetry.example.JaegerExample2");
+        Span span = tracer.spanBuilder("main").startSpan();
+        try (Scope scope = span.makeCurrent()) {
+            doSomething(tracer);
+        } finally {
+            span.end();
+        }
+        Runtime.getRuntime().addShutdownHook(new Thread(tracerProvider::close));
+        TimeUtil.sleepSecond(1);
+    }
+
+    private static void doSomething(Tracer tracer) {
+        Span span = tracer.spanBuilder("doSomething").startSpan();
+        try (Scope scope = span.makeCurrent()) {
+            // Do some work here
+            doSomethingElse(tracer);
+        } finally {
+            span.end();
+        }
+    }
+
+    private static void doSomethingElse(Tracer tracer) {
+        Span span = tracer.spanBuilder("doSomethingElse").startSpan();
+        try (Scope scope = span.makeCurrent()) {
+            // Do some more work here
+
+        } finally {
+            span.end();
+        }
+    }
 ```
 
--- exporter/ar-trace/src/test/java/cn/aishu/exporter/ar_trace/ArExporterTest.java
+++ exporter/ar-trace/src/test/java/cn/aishu/exporter/ar_trace/ArExporterTest.java
@@ -9,7 +9,9 @@ import io.opentelemetry.api.common.AttributeKey;
 import io.opentelemetry.api.common.AttributeType;
 import io.opentelemetry.api.internal.InternalAttributeKeyImpl;
 import io.opentelemetry.api.trace.Span;
+import io.opentelemetry.api.trace.SpanContext;
 import io.opentelemetry.api.trace.Tracer;
+import io.opentelemetry.context.Scope;
 import io.opentelemetry.sdk.OpenTelemetrySdk;
 import io.opentelemetry.sdk.internal.AttributesMap;
 import io.opentelemetry.sdk.resources.Resource;
@@ -22,6 +24,7 @@ import org.junit.Test;
 
 import java.util.List;
 import java.util.concurrent.TimeUnit;
+
 
 public class ArExporterTest {
 
@@ -36,17 +39,14 @@ public class ArExporterTest {
                 SdkTracerProvider.builder()
 //                        .addSpanProcessor(BatchSpanProcessor.builder(LoggingSpanExporter.create()).build())
 
-
+                        //1. 导出到标准输出
                         .addSpanProcessor(SimpleSpanProcessor.create(new ArExporter()))
 
+                        //2. 导出到AnyRobot:注意切换到对应地址：
 //                        .addSpanProcessor(SimpleSpanProcessor.create(ArExporter.create(
 //                                HttpSender.create("http://10.4.15.62/api/feed_ingester/v1/jobs/job-0988e01371fd21c9/events"))))
 
-//                        .addSpanProcessor(SimpleSpanProcessor.create(ArExporter.create(
-//                                HttpsSender.create("https://localhost:8000/api/feed_ingester/v1/jobs/job-4f1931764308121e/events",
-//                                        Retry.create(true,5,15,30),
-//                                        false, 4096))))
-
+                        //3. 导出到AnyRobot:（可配置发送相关参数）
 //                        .addSpanProcessor(SimpleSpanProcessor.create(ArExporter.create(
 //                                HttpSender.create("http://10.4.68.236:13048/api/feed_ingester/v1/jobs/job-4f1931764308121e/events",
 //                                        Retry.create(true,5,15,30),
@@ -67,6 +67,61 @@ public class ArExporterTest {
 
         span.end();
         TimeUtil.sleepSecond(5);
+    }
+
+
+    @Test
+    public  void testChain () {
+        final Resource serviceNameResource =
+                Resource.create(io.opentelemetry.api.common.Attributes.of(ResourceAttributes.SERVICE_NAME, "otel-jaeger-example2"));
+
+        // Set to process the spans by the Jaeger Exporter
+        final SdkTracerProvider tracerProvider =
+                SdkTracerProvider.builder()
+                        .addSpanProcessor(SimpleSpanProcessor.create(new ArExporter()))
+
+                        //发送到anyRobot：
+//                    .addSpanProcessor(SimpleSpanProcessor.create(ArExporter.create(
+//                                HttpSender.create("http://10.4.15.62/api/feed_ingester/v1/jobs/job-0988e01371fd21c9/events",
+//                                        Retry.create(true,5,15,30),
+//                                        true, 4096))))
+
+                        .setResource(Resource.getDefault().merge(serviceNameResource))
+                        .build();
+        final OpenTelemetrySdk openTelemetry =
+                OpenTelemetrySdk.builder().setTracerProvider(tracerProvider).build();
+
+        // it's always a good idea to shut down the SDK cleanly at JVM exit.
+//    Runtime.getRuntime().addShutdownHook(new Thread(tracerProvider::close));
+        Tracer tracer = openTelemetry.getTracer("io.opentelemetry.example.JaegerExample2");
+        Span span = tracer.spanBuilder("main").startSpan();
+        try (Scope scope = span.makeCurrent()) {
+            doSomething(tracer);
+        } finally {
+            span.end();
+        }
+        Runtime.getRuntime().addShutdownHook(new Thread(tracerProvider::close));
+        TimeUtil.sleepSecond(1);
+    }
+
+    private static void doSomething(Tracer tracer) {
+        Span span = tracer.spanBuilder("doSomething").startSpan();
+        try (Scope scope = span.makeCurrent()) {
+            // Do some work here
+            doSomethingElse(tracer);
+        } finally {
+            span.end();
+        }
+    }
+
+    private static void doSomethingElse(Tracer tracer) {
+        Span span = tracer.spanBuilder("doSomethingElse").startSpan();
+        try (Scope scope = span.makeCurrent()) {
+            // Do some more work here
+
+        } finally {
+            span.end();
+        }
     }
 
 }
