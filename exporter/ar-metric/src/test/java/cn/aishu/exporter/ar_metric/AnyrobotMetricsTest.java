fb56b9e42c2f92a79730c7138950d5acaf50de3d
Leal.li <Leal.li@aishu.cn>
2023-04-11 17:05:26 +0800

    更新 Azure Pipelines 的 azure-pipelines.yml

--- exporter/ar-metric/src/test/java/cn/aishu/exporter/ar_metric/AnyrobotMetricsTest.java
+++ exporter/ar-metric/src/test/java/cn/aishu/exporter/ar_metric/AnyrobotMetricsTest.java
@@ -3,8 +3,12 @@ package cn.aishu.exporter.ar_metric;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
+import io.opentelemetry.sdk.common.InstrumentationScopeInfo;
 import io.opentelemetry.sdk.metrics.internal.aggregator.EmptyMetricData;
+import io.opentelemetry.sdk.metrics.internal.data.*;
+import io.opentelemetry.sdk.resources.Resource;
 import org.apache.commons.logging.Log;
+import org.apache.commons.logging.impl.SimpleLog;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -18,6 +22,11 @@ import io.opentelemetry.sdk.metrics.data.MetricDataType;
 @RunWith(PowerMockRunner.class)
 @PrepareForTest({ AnyrobotMetrics.class })
 public class AnyrobotMetricsTest {
+
+    String nameTest = "NameTest";
+    String descriptionTest = "DescriptionTest";
+    String unitTest = "UnitTest";
+
     @Test
     public void testAnyrobotMetricsLongGauge() {
         AnyrobotGauge mockAnyrobotGauge = mock(AnyrobotGauge.class);
@@ -25,9 +34,7 @@ public class AnyrobotMetricsTest {
         Log mockLog = mock(Log.class);
 
         MetricData mockMetricData = mock(MetricData.class);
-        String nameTest = "NameTest";
-        String descriptionTest = "DescriptionTest";
-        String unitTest = "UnitTest";
+
         when(mockMetricData.getName()).thenReturn(nameTest);
         when(mockMetricData.getDescription()).thenReturn(descriptionTest);
         when(mockMetricData.getUnit()).thenReturn(unitTest);
@@ -50,6 +57,36 @@ public class AnyrobotMetricsTest {
         Assert.assertNotNull(testMetrics.getUnit());
         Assert.assertNull(testMetrics.getHistogram());
         Assert.assertNull(testMetrics.getSum());
+    }
+
+    @Test
+    public void testAnyRobotMetrics() {
+        MetricData metricData1 = ImmutableMetricData.createDoubleSum(Resource.getDefault(), InstrumentationScopeInfo.create("test"),"testName", "testDescription", "testUnit", ImmutableSumData.empty());
+        AnyrobotMetrics testMetrics = new AnyrobotMetrics(metricData1, new SimpleLog("test"));
+        Assert.assertNotNull(testMetrics);
+        MetricData metricData2 = ImmutableMetricData.createDoubleGauge(Resource.getDefault(), InstrumentationScopeInfo.create("test"),"testName", "testDescription", "testUnit",  ImmutableGaugeData.empty());
+        AnyrobotMetrics testMetrics2 = new AnyrobotMetrics(metricData2, new SimpleLog("test"));
+        Assert.assertNotNull(testMetrics2);
+        MetricData metricData3 = ImmutableMetricData.createLongSum(Resource.getDefault(), InstrumentationScopeInfo.create("test"),"testName", "testDescription", "testUnit", ImmutableSumData.empty());
+        AnyrobotMetrics testMetrics3 = new AnyrobotMetrics(metricData3, new SimpleLog("test"));
+        Assert.assertNotNull(testMetrics3);
+        MetricData metricData4 = ImmutableMetricData.createLongGauge(Resource.getDefault(), InstrumentationScopeInfo.create("test"),"testName", "testDescription", "testUnit",  ImmutableGaugeData.empty());
+        AnyrobotMetrics testMetrics4 = new AnyrobotMetrics(metricData4, new SimpleLog("test"));
+        Assert.assertNotNull(testMetrics4);
+        MetricData metricData5 = ImmutableMetricData.createDoubleSummary(Resource.getDefault(), InstrumentationScopeInfo.create("test"),"testName", "testDescription", "testUnit", ImmutableSummaryData.empty());
+        AnyrobotMetrics testMetrics5 = new AnyrobotMetrics(metricData5, new SimpleLog("test"));
+        Assert.assertNotNull(testMetrics5);
+        MetricData metricData6 = ImmutableMetricData.createExponentialHistogram(Resource.getDefault(), InstrumentationScopeInfo.create("test"),"testName", "testDescription", "testUnit", ImmutableExponentialHistogramData.empty());
+        AnyrobotMetrics testMetrics6 = new AnyrobotMetrics(metricData6, new SimpleLog("test"));
+        Assert.assertNotNull(testMetrics6);
+        testMetrics.setName(nameTest);
+        testMetrics.setDescription(descriptionTest);
+        Assert.assertEquals(nameTest, testMetrics.getName());
+        Assert.assertEquals(descriptionTest, testMetrics.getDescription());
+        Assert.assertNull(testMetrics.getGauge());
+        Assert.assertNotNull(testMetrics.getUnit());
+        Assert.assertNull(testMetrics.getHistogram());
+        Assert.assertNotNull(testMetrics.getSum());
     }
 
 
